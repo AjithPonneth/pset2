a6c9f34aee72f8cf2e23879d23b20f6239582536
aponneth <aponneth@cisco.com>
2022-07-06 13:39:41 +0530

    Adding cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='farmer reaction to climate change',
+    author='Ajith',
+    license='MIT',
+)
